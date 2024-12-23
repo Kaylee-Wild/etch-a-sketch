ac70aeca07588f8146a8065b9471fe4aa53df799
Kaylee-Wild <wildkaylee13@gmail.com>
2024-12-23 14:52:43 -0700

    eventListener changes background color of ALL cells

--- scripts.js
+++ scripts.js
@@ -2,7 +2,23 @@ const grid = document.querySelector('.grid');
 
 for (let i = 0; i < 256; i++) {
     const cells = document.createElement('div');
-    cells.classList.add('cell');
+    cells.classList.add('cells');
     grid.appendChild(cells);
-}
+    
 
+    //changing the css class isn't working to change the background color of the div
+    //going to try DOM style background change instead
+    
+    document.querySelectorAll('.cells').forEach((item) => {
+        item.addEventListener('mouseover', () => {
+            cells.style.backgroundColor = "blue"
+            //changes the background of ALL cells, want it to change just the one
+
+            
+        })
+    })
+    
+
+
+
+}
