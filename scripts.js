9c28988314f1e46814b1cfa33d68c0ee4e89d559
Kaylee-Wild <wildkaylee13@gmail.com>
2025-01-02 14:47:57 -0700

    individual cells colors change when moused over

--- scripts.js
+++ scripts.js
@@ -5,20 +5,9 @@ for (let i = 0; i < 256; i++) {
     cells.classList.add('cells');
     grid.appendChild(cells);
     
-
-    //changing the css class isn't working to change the background color of the div
-    //going to try DOM style background change instead
-    
-    document.querySelectorAll('.cells').forEach((item) => {
-        item.addEventListener('mouseover', () => {
-            cells.style.backgroundColor = "blue"
-            //changes the background of ALL cells, want it to change just the one
-
-            
-        })
+    cells.addEventListener('mouseover', () => {
+        cells.style.backgroundColor = "blue"
     })
-    
-
 
 
 }
