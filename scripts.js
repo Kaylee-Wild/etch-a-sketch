98f9e24bc8c05ec8b75930336c5366754294016e
Kaylee-Wild <wildkaylee13@gmail.com>
2025-01-06 21:26:20 -0700

    started working on collecting user input to set grid size

--- scripts.js
+++ scripts.js
@@ -1,13 +1,33 @@
 const grid = document.querySelector('.grid');
 
-for (let i = 0; i < 256; i++) {
-    const cells = document.createElement('div');
-    cells.classList.add('cells');
-    grid.appendChild(cells);
+function changeGridSize() {
+    let rowNum = prompt("Enter the size of the grid you want");
+    //user will enter a number
     
-    cells.addEventListener('mouseover', () => {
-        cells.style.backgroundColor = "blue"
+    return parseInt(rowNum);
+    //return user's number
+};
+
+    let rowNum = changeGridSize();
+
+    const cellsPerRow = rowNum;
+    const totalCells = rowNum * rowNum;
+
+    const cellSize = (100 / cellsPerRow) - 1;
+
+grid.innerHTML = ' ';
+
+for (let i = 0; i < totalCells; i++) {
+    const cellStyle = document.createElement('div');
+    cellStyle.classList.add('cellStyle');
+
+    cellStyle.style.width = `${cellSize}%`
+    cellStyle.style.height = `${cellSize}%`
+
+    grid.appendChild(cellStyle);
+
+    cellStyle.addEventListener('mouseover', () => {
+        cellStyle.style.backgroundColor = "blue"
     })
-
-
 }
+
